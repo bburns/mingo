acc5647254ffdef63702491e179c4b3625ce89e3
Francis Asante <kofrasa@gmail.com>
2024-10-22 13:46:39 -0700

    refactor: Update setDifference to use ValueMap

--- src/operators/expression/set/setDifference.ts
+++ src/operators/expression/set/setDifference.ts
@@ -4,7 +4,7 @@
 
 import { computeValue, ExpressionOperator, Options } from "../../../core";
 import { Any, AnyObject } from "../../../types";
-import { notInArray } from "../../../util";
+import { ValueMap } from "../../../util";
 
 /**
  * Returns elements of a set that do not appear in a second set.
@@ -17,5 +17,8 @@ export const $setDifference: ExpressionOperator = (
   options: Options
 ): Any => {
   const args = computeValue(obj, expr, null, options) as Any[][];
-  return args[0].filter(v => notInArray(args[1], v));
+  const m = ValueMap.init(options.hashFunction);
+  args[0].forEach(v => m.set(v, true));
+  args[1].forEach(v => m.delete(v));
+  return Array.from(m.keys());
 };
