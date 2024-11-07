cd0cdba3f1bb6855df112853e5eac9ae443ee4db
Francis Asante <kofrasa@gmail.com>
2024-11-06 22:37:24 -0500

    fix($subtract): Handle dates and add input validation

--- src/operators/expression/arithmetic/subtract.ts
+++ src/operators/expression/arithmetic/subtract.ts
@@ -1,7 +1,11 @@
 // Arithmetic Expression Operators: https://docs.mongodb.com/manual/reference/operator/aggregation/#arithmetic-expression-operators
 
+import { assert } from "console";
+import { isDate } from "util/types";
+
 import { computeValue, ExpressionOperator, Options } from "../../../core";
 import { Any, AnyObject } from "../../../types";
+import { isNumber } from "../../../util";
 
 /**
  * Takes an array that contains two numbers or two dates and subtracts the second value from the first.
@@ -14,7 +18,13 @@ export const $subtract: ExpressionOperator = (
   obj: AnyObject,
   expr: Any,
   options: Options
-): number => {
-  const args = computeValue(obj, expr, null, options) as number[];
-  return args[0] - args[1];
+): Any => {
+  const args = computeValue(obj, expr, null, options) as Any[];
+  assert(
+    args.every(n => isNumber(n) || isDate(n)),
+    "$substrat operands must resolve to number or date."
+  );
+  const foundDate = args.some(isDate);
+  const [a, b] = args;
+  return foundDate ? new Date(+a - +b) : +a - +b;
 };
