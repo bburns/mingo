9c854b0c2e2f9c45681bca9f1d22629579cd596b
Francis Asante <kofrasa@gmail.com>
2025-09-25 12:08:02 -0700

    refactor(browser): change browser entry point to use full context builder

--- src-browser/index.ts
+++ src-browser/index.ts
@@ -1,4 +1,46 @@
-/** Entry point for browser bundle. Includes all operators. */
-import "../src/init/system";
+import { AggregatorImpl } from "../src/aggregator/_internal";
+import { Context, initOptions, Options, UpdateOptions } from "../src/core";
+import { Cursor } from "../src/cursor";
+import fullContext from "../src/init/context";
+import { Source } from "../src/lazy";
+import { QueryImpl } from "../src/query/_internal";
+import { AnyObject } from "../src/types";
+import { createUpdater as createUpdaterInternal } from "../src/updater";
 
-export * from "../src/index";
+export { Context, ProcessingMode } from "../src/core";
+
+const context = fullContext();
+const makeOpts = (options?: Partial<Options>) => {
+  const opts = initOptions(options);
+  return { ...opts, context: Context.merge(context, opts.context) };
+};
+export class Query extends QueryImpl {
+  constructor(condition: AnyObject, options?: Partial<Options>) {
+    super(condition, makeOpts(options));
+  }
+}
+
+export class Aggregator extends AggregatorImpl {
+  constructor(pipeline: AnyObject[], options?: Partial<Options>) {
+    super(pipeline, makeOpts(options));
+  }
+}
+
+export const createUpdater = (defaultOptions?: UpdateOptions) =>
+  createUpdaterInternal({
+    ...defaultOptions,
+    queryOptions: makeOpts(defaultOptions?.queryOptions)
+  });
+
+export const aggregate = (
+  collection: Source,
+  pipeline: AnyObject[],
+  options?: Partial<Options>
+): AnyObject[] => new Aggregator(pipeline, options).run(collection);
+
+export const find = <T>(
+  collection: Source,
+  criteria: AnyObject,
+  projection?: AnyObject,
+  options?: Partial<Options>
+): Cursor<T> => new Query(criteria, options).find<T>(collection, projection);
